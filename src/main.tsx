5c250817447e6ee280dd9612cc4e666342d73afe
Lana Ramadan <lanamramadan@gmail.com>
2025-07-25 22:31:20 -0700

    Fixing calcite components + tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
+
+import '@esri/calcite-components/dist/calcite/calcite.css'
+import { defineCustomElements } from "@esri/calcite-components/dist/loader";
+
+import React from 'react'
+defineCustomElements(window);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
