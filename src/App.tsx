8cce597c431e6ef023704eef3b0d48469d77e9a2
Lana Ramadan <lanamramadan@gmail.com>
2025-07-26 11:27:21 -0700

    Updating styling

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MapDashboard />} />
+        <Route path="/map" element={<MapDashboard />} />
       </Routes>
     </Router>
   )
--- src/components/customMap.tsx
+++ src/components/customMap.tsx
@@ -11,37 +11,12 @@ import Graphic from "@arcgis/core/Graphic.js";
 
 function CustomMap() {
   const handleViewReady = (event: any) => {
-    const viewElement = event.target;
-
-    const point = {
-      type: "point",
-      longitude: -118.38,
-      latitude: 33.34,
-    };
-
-    const markerSymbol = {
-      type: "simple-marker",
-      style: "triangle",
-      size: 15,
-      color: "red",
-      outline: {
-        color: "white",
-        width: 2,
-      },
-    };
-
-    const pointGraphic = new Graphic({
-      geometry: point,
-      symbol: markerSymbol,
-    });
-
-    viewElement.graphics.add(pointGraphic);
   };
 
   return (
-    <arcgis-map item-id="02b37471d5d84cacbebcccd785460e94" onarcgisViewReadyChange={handleViewReady}  style={{ width: "100%", height: "100vh" }}>
+    <arcgis-map item-id="76862b21eceb495da6a60941ce12bbd5" onarcgisViewReadyChange={handleViewReady}  style={{ width: "100%", height: "100vh" }}>
       <arcgis-search position="bottom-left" />
-      {/* <arcgis-legend position="bottom-right" /> */}
+      <arcgis-legend position="bottom-right" />
       <arcgis-zoom position="bottom-right" />
     </arcgis-map>
   );
